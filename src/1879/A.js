63fe3f237c6285fab72b1d5112284e8c26cc24bc
kuzanatoliy <njkznu@gmail.com>
2023-10-18 07:59:42 +0300

    feat: JTN-1879A add solution

---
+++ src/1879/A.js
@@ -0,0 +1,10 @@
+module.exports = {
+  shouldFirstWin: ([first, ...others]) => {
+    for (var i = 0; i < s.length; i++) {
+      if (others[ii][1] >= first[1] && others[ii][0] >= first[0]) {
+        return -1;
+      }
+      return first[0];
+    }
+  },
+};
