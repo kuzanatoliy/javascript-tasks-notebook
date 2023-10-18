fcc33f848a444337652d636e3183ef701937117c
kuzanatoliy <njkznu@gmail.com>
2023-10-18 08:24:06 +0300

    fix(solution): JTN-1879A test description

--- src/1879/A.spec.js
+++ src/1879/A.spec.js
@@ -3,10 +3,10 @@ const { shouldFirstWin } = require('./A');
 describe('1879A. Rigged!', () => {
   it.each`
     data                                | result
-    ${[[7, 4], [9, 3], [4, 6], [2, 2]]} | ${7}
+    ${[[7, 4], [9, 3], [4, 6], [2, 2]]} | ${-1}
     ${[[4, 6], [100, 100]]}             | ${-1}
     ${[[1337, 3], [1337, 3]]}           | ${-1}
-  `('Base test: input $node output $result', ({ data, result }) => {
+  `('Base test: input $data output $result', ({ data, result }) => {
     expect(shouldFirstWin(data)).toBe(result);
   });
 });
