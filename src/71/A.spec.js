016dcb3fcc89686bf31e8cf3710a7a0bd1f03f0a
kuzanatoliy <njkznu@gmail.com>
2023-10-19 22:54:03 +0300

    feat(solution): JTN-71A coverage

---
+++ src/71/A.spec.js
@@ -0,0 +1,13 @@
+const { convertLongWord } = require('./A');
+
+describe('71A. Way Too Long Words', () => {
+  it.each`
+    n    | word                                               | result
+    ${1} | ${'word'}                                          | ${'word'}
+    ${2} | ${'localization'}                                  | ${'l10n'}
+    ${3} | ${'internationalization'}                          | ${'i18n'}
+    ${4} | ${'pneumonoultramicroscopicsilicovolcanoconiosis'} | ${'p43s'}
+  `('Base test: $n', ({ word, result }) => {
+    expect(checkEvenPices(word)).toBe(result);
+  });
+});
