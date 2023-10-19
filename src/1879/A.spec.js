76c07d61bbafc7f902ae7058dca356172e6354f5
kuzanatoliy <njkznu@gmail.com>
2023-10-19 07:44:51 +0300

    fix: JTN-6 test descriptions

--- src/1879/A.spec.js
+++ src/1879/A.spec.js
@@ -2,11 +2,11 @@ const { shouldFirstWin } = require('./A');
 
 describe('1879A. Rigged!', () => {
   it.each`
-    data                                | result
-    ${[[7, 4], [9, 3], [4, 6], [2, 2]]} | ${7}
-    ${[[4, 6], [100, 100]]}             | ${-1}
-    ${[[1337, 3], [1337, 3]]}           | ${-1}
-  `('Base test: input $data output $result', ({ data, result }) => {
+    n    | data                                | result
+    ${1} | ${[[7, 4], [9, 3], [4, 6], [2, 2]]} | ${7}
+    ${2} | ${[[4, 6], [100, 100]]}             | ${-1}
+    ${3} | ${[[1337, 3], [1337, 3]]}           | ${-1}
+  `('Base test: $n', ({ data, result }) => {
     expect(shouldFirstWin(data)).toBe(result);
   });
 });
