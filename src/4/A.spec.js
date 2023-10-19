41a4a3ceed8939c6d6ec088476b271b64c942cfa
kuzanatoliy <njkznu@gmail.com>
2023-10-19 22:33:33 +0300

    feat(solution): JTN-4A coverage

---
+++ src/4/A.spec.js
@@ -0,0 +1,19 @@
+const { checkEvenPices } = require('./A');
+
+describe('4A. Watermelon', () => {
+  it.each`
+    n    | weight | result
+    ${1} | ${8}   | ${'YES'}
+  `('Base test: $n', ({ weight, result }) => {
+    expect(checkEvenPices(weight)).toBe(result);
+  });
+
+  it.each`
+    n    | weight | result
+    ${1} | ${2}   | ${'NO'}
+    ${2} | ${3}   | ${'NO'}
+    ${2} | ${6}   | ${'YES'}
+  `('Special test: $n', ({ weight, result }) => {
+    expect(checkEvenPices(weight)).toBe(result);
+  });
+});
