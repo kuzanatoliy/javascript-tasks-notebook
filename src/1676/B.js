72739704934b98f35c5e474de7ace6f7b9eb2836
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-15 07:49:26 +0300

    feat(solution): JTN-1676B

---
+++ src/1676/B.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  getCountOfCandies: (boxes) => {
+    const min = Math.min(...boxes);
+    let sum = 0;
+    for (let j = 0; j < boxes.length; j++) {
+      sum += boxes[j] - min;
+    }
+    return sum;
+  },
+};
---
+++ src/1676/B.spec.js
@@ -0,0 +1,14 @@
+﻿const { getCountOfCandies } = require('./B');
+
+describe('1676B. Equal Candies', () => {
+  it.each`
+    n    | boxes                                | result
+    ${1} | ${[1, 2, 3, 4, 5]}                   | ${10}
+    ${2} | ${[1000, 1000, 5, 1000, 1000, 1000]} | ${4975}
+    ${3} | ${[1, 2, 3, 5, 1, 2, 7, 9, 13, 5]}   | ${38}
+    ${4} | ${[8, 8, 8]}                         | ${0}
+    ${5} | ${[10000000]}                        | ${0}
+  `('Base test: $n', ({ boxes, result }) => {
+    expect(getCountOfCandies(boxes)).toBe(result);
+  });
+});
