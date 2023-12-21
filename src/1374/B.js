ece5364031b6d711e6f1009e9f88448287003df6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-21 08:17:33 +0300

    feat(solution): JTN-1374B

---
+++ src/1374/B.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+  getMinNumber: (number) => {
+    let count = 0;
+    while (number > 1 && number % 3 === 0) {
+      if (number % 6) {
+        number *= 2;
+      } else {
+        number /= 6;
+      }
+      count++;
+    }
+    return number > 1 ? -1 : count;
+  },
+};
---
+++ src/1374/B.spec.js
@@ -0,0 +1,16 @@
+﻿const { getMinNumber } = require('./B');
+
+describe('1374B. Multiply by 2, divide by 6', () => {
+  it.each`
+    n    | number       | result
+    ${1} | ${1}         | ${0}
+    ${2} | ${2}         | ${-1}
+    ${3} | ${3}         | ${2}
+    ${4} | ${12}        | ${-1}
+    ${5} | ${12345}     | ${-1}
+    ${6} | ${15116544}  | ${12}
+    ${7} | ${387420489} | ${36}
+  `('Base test: $n', ({ number, result }) => {
+    expect(getMinNumber(number)).toBe(result);
+  });
+});
