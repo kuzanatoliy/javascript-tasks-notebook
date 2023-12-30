5f9e2bbdff7e4ec922159e439ebaf0eac9864c9c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-30 09:26:10 +0300

    feat(solution): JTN-1551B1

---
+++ src/1551/B1.js
@@ -0,0 +1,25 @@
+﻿module.exports = {
+  getCountOfRedItems: (str) => {
+    const l = new Array(26).fill(0);
+
+    for (let j = 0; j < str.length; j++) {
+      l[str[j].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+
+    let c1 = 0;
+    let c2 = 0;
+
+    for (let j = 0; j < l.length; j++) {
+      if (!l[j]) {
+        continue;
+      }
+      if (l[j] > 1) {
+        c1++;
+        continue;
+      }
+      c2++;
+    }
+
+    return Math.floor(c1 + c2 / 2);
+  },
+};
---
+++ src/1551/B1.spec.js
@@ -0,0 +1,14 @@
+﻿const { getCountOfRedItems } = require('./B1');
+
+describe('1551A. Polycarp and str', () => {
+  it.each`
+    n    | str             | result
+    ${1} | ${'kzaaa'}      | ${2}
+    ${2} | ${'codeforces'} | ${5}
+    ${3} | ${'archive'}    | ${3}
+    ${4} | ${'y'}          | ${0}
+    ${5} | ${'xxxxxx'}     | ${1}
+  `('Base test: $n', ({ str, result }) => {
+    expect(getCountOfRedItems(str)).toStrictEqual(result);
+  });
+});
