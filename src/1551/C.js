20fce5f2fd482590ddab5a964c1ea3edc7926ef5
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-30 10:02:35 +0300

    feat(solution): JTN-1551C

---
+++ src/1551/C.js
@@ -0,0 +1,49 @@
+﻿/* eslint-disable max-depth */
+module.exports = {
+  getCountOfWordsInInterestingStory: (strings) => {
+    const l = new Set();
+    let bc = 0;
+
+    for (let j = 0; j < strings.length; j++) {
+      for (let k = 0; k < strings[j].length; k++) {
+        l.add(strings[j][k]);
+      }
+    }
+
+    for (const item of l) {
+      let a = 0;
+      let b = 0;
+      const ans = [];
+      for (let j = 0; j < strings.length; j++) {
+        let la = 0;
+        let lb = 0;
+        for (let k = 0; k < strings[j].length; k++) {
+          if (strings[j][k] === item) {
+            la++;
+          }
+        }
+        lb = strings[j].length - la;
+        ans.push([la, lb, lb - la]);
+        a += la;
+        b += lb;
+      }
+      if (a > b) {
+        bc = strings.length;
+        break;
+      }
+      ans.sort((item1, item2) => item1[2] - item2[2]);
+      let t = strings.length;
+      while (t && t > bc) {
+        t--;
+        a -= ans[t][0];
+        b -= ans[t][1];
+        if (a > b) {
+          bc = t;
+          break;
+        }
+      }
+    }
+
+    return bc;
+  },
+};
---
+++ src/1551/C.spec.js
@@ -0,0 +1,15 @@
+﻿const { getCountOfWordsInInterestingStory } = require('./C');
+
+describe('1551C. Interesting Story', () => {
+  it.each`
+    n    | strings                          | result
+    ${1} | ${['bac', 'aaada', 'e']}         | ${3}
+    ${2} | ${['aba', 'abcde', 'aba']}       | ${2}
+    ${3} | ${['baba', 'baba']}              | ${0}
+    ${4} | ${['ab', 'ab', 'c', 'bc']}       | ${2}
+    ${5} | ${['cbdca', 'd', 'a', 'd', 'e']} | ${3}
+    ${6} | ${['b', 'c', 'ca']}              | ${2}
+  `('Base test: $n', ({ strings, result }) => {
+    expect(getCountOfWordsInInterestingStory(strings)).toStrictEqual(result);
+  });
+});
