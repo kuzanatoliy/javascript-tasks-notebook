34fc1ebe6e27944a76caf842770a8b74cdc50182
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-01-03 08:16:17 +0300

    feat(solution): JTN-1323B

---
+++ src/1353/B.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+  getMaxPossibleSum: (countOfMoves, arrayA, arrayB) => {
+    let sum = 0;
+    let j;
+    arrayA.sort((a, b) => a - b);
+    arrayB.sort((a, b) => b - a);
+    for (j = 0; j < countOfMoves && arrayA[j] - arrayB[j] < 0; j++) {
+      sum += arrayB[j];
+    }
+    while (j < arrayA.length) {
+      sum += arrayA[j];
+      j++;
+    }
+    return sum;
+  },
+};
---
+++ src/1353/B.spec.js
@@ -0,0 +1,14 @@
+﻿const { getMaxPossibleSum } = require('./B');
+
+describe('1353B. Two Arrays And Swaps', () => {
+  it.each`
+    n    | countOfMoves | arrayA             | arrayB                | result
+    ${1} | ${1}         | ${[1, 2]}          | ${[3, 4]}             | ${6}
+    ${2} | ${5}         | ${[5, 5, 6, 6, 5]} | ${[1, 2, 5, 4, 3]}    | ${27}
+    ${3} | ${3}         | ${[1, 2, 3, 4, 5]} | ${[10, 9, 10, 10, 9]} | ${39}
+    ${4} | ${0}         | ${[2, 2, 4, 3]}    | ${[2, 4, 2, 3]}       | ${11}
+    ${5} | ${4}         | ${[1, 2, 2, 1]}    | ${[4, 4, 5, 4]}       | ${17}
+  `('Base test: $n', ({ countOfMoves, arrayA, arrayB, result }) => {
+    expect(getMaxPossibleSum(countOfMoves, arrayA, arrayB)).toBe(result);
+  });
+});
