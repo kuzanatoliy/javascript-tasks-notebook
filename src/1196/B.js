6f5981df4646102c7fdc61349a236b6ed378c8d6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-01-08 08:04:31 +0300

    feat(solution): JTN-1196B

---
+++ src/1196/B.js
@@ -0,0 +1,25 @@
+﻿module.exports = {
+  isItPossibleDivideToSubsegments: (segmentsCount, numbers) => {
+    const elems = [];
+
+    numbers.forEach((item, index) => {
+      if (item % 2 > 0) {
+        elems.push(index);
+      }
+    });
+
+    if (
+      elems.length < segmentsCount ||
+      elems.length % 2 !== segmentsCount % 2
+    ) {
+      return ['NO'];
+    } else {
+      const res = [];
+      for (let j = 0; j < segmentsCount - 1; j++) {
+        res.push(elems[j] + 1);
+      }
+      res.push(numbers.length);
+      return ['YES', res];
+    }
+  },
+};
---
+++ src/1196/B.spec.js
@@ -0,0 +1,14 @@
+﻿const { isItPossibleDivideToSubsegments } = require('./B');
+
+describe('1196B. Odd Sum Segments', () => {
+  it.each`
+    n    | segmentsCount | numbers                | result
+    ${1} | ${3}          | ${[7, 18, 3, 14, 1]}   | ${['YES', [1, 3, 5]]}
+    ${2} | ${4}          | ${[1, 2, 3, 4, 5]}     | ${['NO']}
+    ${3} | ${2}          | ${[1, 2, 8, 4, 10, 2]} | ${['NO']}
+  `('Base test: $n', ({ segmentsCount, numbers, result }) => {
+    expect(
+      isItPossibleDivideToSubsegments(segmentsCount, numbers)
+    ).toStrictEqual(result);
+  });
+});
