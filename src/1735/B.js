189124db5c93de73a097fb3e5cdce807e757f758
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-01-18 07:51:43 +0300

    feat(solution): JTN-1735B

---
+++ src/1735/B.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  getMinNumberOfSteps: (numbers) => {
+    const tab = numbers[0] * 2 - 1;
+    let count = 0;
+    for (let j = 0; j < numbers.length; j++) {
+      count += Math.ceil(numbers[j] / tab) - 1;
+    }
+    return count;
+  },
+};
---
+++ src/1735/B.spec.js
@@ -0,0 +1,12 @@
+﻿const { getMinNumberOfSteps } = require('./B');
+
+describe('1730B. Tea with Tangerines', () => {
+  it.each`
+    n    | numbers                         | result
+    ${1} | ${[1, 2, 3, 4, 5]}              | ${10}
+    ${2} | ${[1033]}                       | ${0}
+    ${3} | ${[600, 900, 1300, 2000, 2550]} | ${4}
+  `('Base test: $n', ({ numbers, result }) => {
+    expect(getMinNumberOfSteps(numbers)).toBe(result);
+  });
+});
