23203d5941e8bfd333b8ebbc523e04d0e465f470
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-02 15:06:25 +0300

    feat(solution): JTN-1920A

---
+++ src/1920/A.js
@@ -0,0 +1,33 @@
+﻿/* eslint-disable prefer-destructuring */
+module.exports = {
+  getNumbers: (constraints) => {
+    let min = Number.MAX_SAFE_INTEGER;
+    let max = Number.MIN_SAFE_INTEGER;
+    for (let i = 0; i < constraints.length; i++) {
+      if (constraints[i][0] === 1 && constraints[i][1] > max) {
+        max = constraints[i][1];
+      }
+      if (constraints[i][0] === 2 && constraints[i][1] < min) {
+        min = constraints[i][1];
+      }
+    }
+
+    if (max > min) {
+      return 0;
+    }
+
+    const s = new Set();
+
+    for (let i = 0; i < constraints.length; i++) {
+      if (
+        constraints[i][0] === 3 &&
+        constraints[i][1] <= min &&
+        constraints[i][1] >= max
+      ) {
+        s.add(constraints[i][1]);
+      }
+    }
+
+    return min - max + 1 - s.size;
+  },
+};
---
+++ src/1920/README.md
@@ -0,0 +1,17 @@
+# Codeforces Round 919 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/1920)
+
+[A. Satisfying Constraints](https://codeforces.com/contest/1920/problem/A) - Done
+
+[B. Summation Game](https://codeforces.com/contest/1920/problem/B)
+
+[C. Partitioning the Array](https://codeforces.com/contest/1920/problem/C)
+
+[D. Array Repetition](https://codeforces.com/contest/1920/problem/D)
+
+[E. Counting Binary Strings](https://codeforces.com/contest/1920/problem/E)
+
+[F1. Smooth Sailing (Easy Version)](https://codeforces.com/contest/1920/problem/F1)
+
+[F2. Smooth Sailing (Hard Version)](https://codeforces.com/contest/1920/problem/F2)
