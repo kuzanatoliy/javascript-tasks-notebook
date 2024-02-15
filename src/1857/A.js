01fbecd074c84af9cce32525989cf8a7e70f10c9
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-15 19:14:21 +0300

    feat(solution): JTN-1857A

---
+++ src/1857/A.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  isItPossibleToColor: (array) => {
+    let count = 0;
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] % 2 === 1) {
+        count++;
+      }
+    }
+    return count % 2 === 0 ? 'YES' : 'NO';
+  },
+};
---
+++ src/1857/A.spec.js
@@ -0,0 +1,16 @@
+﻿const { isItPossibleToColor } = require('./A');
+
+describe('1850A. To My Critics', () => {
+  it.each`
+    n    | array                       | result
+    ${1} | ${[1, 2, 4, 3, 2, 3, 5, 4]} | ${'YES'}
+    ${2} | ${[4, 7]}                   | ${'NO'}
+    ${3} | ${[3, 9, 8]}                | ${'YES'}
+    ${4} | ${[1, 7]}                   | ${'YES'}
+    ${5} | ${[5, 4, 3, 2, 1]}          | ${'NO'}
+    ${6} | ${[4, 3, 4, 5]}             | ${'YES'}
+    ${7} | ${[50, 48]}                 | ${'YES'}
+  `('Base test: $n', ({ array, result }) => {
+    expect(isItPossibleToColor(array)).toBe(result);
+  });
+});
