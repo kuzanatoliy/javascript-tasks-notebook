1f38569f7a098f13a3abd2792671c4b2ef52e1f5
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-17 22:44:43 +0300

    feat(solution): JTN-1791C

---
+++ src/1791/C.spec.js
@@ -0,0 +1,18 @@
+﻿const { getCountOfOperation } = require('./C');
+
+describe('1791C. Prepend and Append', () => {
+  it.each`
+    n    | string          | result
+    ${1} | ${'100'}        | ${1}
+    ${2} | ${'0111'}       | ${2}
+    ${3} | ${'10101'}      | ${5}
+    ${4} | ${'101010'}     | ${0}
+    ${5} | ${'1010110'}    | ${3}
+    ${6} | ${'1'}          | ${1}
+    ${7} | ${'10'}         | ${0}
+    ${8} | ${'11'}         | ${2}
+    ${9} | ${'1011011010'} | ${4}
+  `('Base test: $n', ({ string, result }) => {
+    expect(getCountOfOperation(string)).toBe(result);
+  });
+});
