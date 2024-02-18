99b296f6d3dca52e714608a39e02c9f95940c514
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-18 15:35:53 +0300

    feat(solution): JTN-1932B

---
+++ src/1932/B.spec.js
@@ -0,0 +1,13 @@
+﻿const { getYear } = require('./B');
+
+describe('1932B', () => {
+  it.each`
+    n    | array                            | result
+    ${1} | ${[3, 2, 4, 5, 9, 18]}           | ${36}
+    ${2} | ${[1, 2, 3, 4, 5]}               | ${5}
+    ${3} | ${[1, 1, 1, 1, 1]}               | ${5}
+    ${4} | ${[50, 30, 711, 200, 503, 1006]} | ${2012}
+  `('Base test: $n', ({ array, result }) => {
+    expect(getYear(array)).toBe(result);
+  });
+});
