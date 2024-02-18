1c4e73fa72ebf5f1497a22de049956347951f2df
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-18 16:01:30 +0300

    feat(solution): JTN-1932C

---
+++ src/1932/C.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+  getMods: (mod, array, operations) => {
+    let p = 1;
+    for (let j = 0; j < array.length; j++) {
+      p *= array[j];
+    }
+    const res = [p % mod];
+    let l = 0;
+    let r = array.length - 1;
+    for (let j = 0; j < operations.length - 1; j++) {
+      if (operations[j] === 'L') {
+        p /= array[l];
+        l++;
+      }
+      if (operations[j] === 'R') {
+        p /= array[r];
+        r--;
+      }
+      res.push(p % mod);
+    }
+    return res;
+  },
+};
