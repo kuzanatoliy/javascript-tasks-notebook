3db06bce99c27bd479cbe837749cbd3504384baf
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-18 16:29:18 +0300

    refactor(solution): JTN-1932C

--- src/1932/C.js
+++ src/1932/C.js
@@ -1,23 +1,30 @@
 ﻿module.exports = {
   getMods: (mod, array, operations) => {
-    let p = 1;
-    for (let j = 0; j < array.length; j++) {
-      p *= array[j];
-    }
-    const res = [p % mod];
     let l = 0;
-    let r = array.length - 1;
-    for (let j = 0; j < operations.length - 1; j++) {
+    let r = array.length;
+    for (let j = 0; j < operations.length; j++) {
       if (operations[j] === 'L') {
-        p /= array[l];
         l++;
       }
       if (operations[j] === 'R') {
-        p /= array[r];
         r--;
       }
-      res.push(p % mod);
     }
-    return res;
+    l--;
+    let p = 1;
+    const res = [];
+    for (let j = operations.length - 1; j >= 0; j--) {
+      if (operations[j] === 'L') {
+        p = (p * array[l]) % mod;
+        l--;
+        res.push(p);
+      }
+      if (operations[j] === 'R') {
+        p = (p * array[r]) % mod;
+        r++;
+        res.push(p);
+      }
+    }
+    return res.reverse();
   },
 };
