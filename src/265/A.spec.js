15e9e2b187a53df10f0e81645881a610b289b6a7
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-03-02 11:56:59 +0300

    feat(solution): JTN-265A

---
+++ src/265/A.spec.js
@@ -0,0 +1,12 @@
+﻿const { getPosition } = require('./A');
+
+describe('265A. Colorful Stones (Simplified Edition)', () => {
+  it.each`
+    n    | string                                                  | instructions                                            | result
+    ${1} | ${'RGB'}                                                | ${'RRR'}                                                | ${2}
+    ${2} | ${'RRRBGBRBBB'}                                         | ${'BBBRR'}                                              | ${3}
+    ${3} | ${'BRRBGBRGRBGRGRRGGBGBGBRGBRGRGGGRBRRRBRBBBGRRRGGBBB'} | ${'BBRBGGRGRGBBBRBGRBRBBBBRBRRRBGBBGBBRRBBGGRBRRBRGRB'} | ${15}
+  `('Base test: $n', ({ string, instructions, result }) => {
+    expect(getPosition(string, instructions)).toBe(result);
+  });
+});
---
+++ src/265/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 162 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/265)
+
+[A. Colorful Stones (Simplified Edition)](https://codeforces.com/contest/265/problem/A) - Done
+
+[B. Roadside Trees (Simplified Edition)](https://codeforces.com/contest/265/problem/B)
+
+[C. Escape from Stones](https://codeforces.com/contest/265/problem/C)
+
+[D. Good Sequences](https://codeforces.com/contest/265/problem/D)
+
+[E. Choosing Balls](https://codeforces.com/contest/265/problem/E)
