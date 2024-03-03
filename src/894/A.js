8ca2ce75d3751e76a2807f2a8e260d67178f0f73
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-03-03 21:01:50 +0300

    feat(solution): JTN-894A

---
+++ src/894/A.js
@@ -0,0 +1,16 @@
+﻿/* eslint-disable max-depth */
+module.exports = {
+  getCombinations: (string) => {
+    let d = 0;
+    for (let c = 0; c < string.length; c++) {
+      for (let j = c + 1; j < string.length; j++) {
+        for (let l = j + 1; l < string.length; l++) {
+          if (string[c] === 'Q' && string[j] === 'A' && string[l] === 'Q') {
+            d++;
+          }
+        }
+      }
+    }
+    return d;
+  },
+};
