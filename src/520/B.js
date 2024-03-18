5d91453b75b6ebfc45754a635e056464982ffd35
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-03-18 18:27:29 +0300

    feat(solution): JTN-520B

---
+++ src/520/B.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  getCountOfMoves: (a, b) => {
+    let i = 0;
+    while (a < b) {
+      b = b % 2 === 1 ? b + 1 : b / 2;
+      i++;
+    }
+    return i + a - b;
+  },
+};
---
+++ src/520/B.spec.js
@@ -0,0 +1,12 @@
+﻿const { getCountOfMoves } = require('./B');
+
+describe('520B. Two Buttons', () => {
+  it.each`
+    n    | a     | b      | result
+    ${1} | ${10} | ${1}   | ${9}
+    ${2} | ${4}  | ${6}   | ${2}
+    ${3} | ${99} | ${100} | ${50}
+  `('Base test: $n', ({ a, b, result }) => {
+    expect(getCountOfMoves(a, b)).toBe(result);
+  });
+});
