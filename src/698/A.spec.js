45f170baf28e080fbb1c5777feac9d1562f0abf9
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-03-30 09:32:32 +0300

    feat(solution): JTN-698A

---
+++ src/698/A.spec.js
@@ -0,0 +1,11 @@
+﻿const { getResult } = require('./A');
+
+describe('686A. Free Ice Cream', () => {
+  it.each`
+    n    | initCount | iterations                                                 | result
+    ${1} | ${7}      | ${[['+', 5], ['-', 10], ['-', 20], ['+', 40], ['-', 20]]}  | ${[22, 1]}
+    ${2} | ${17}     | ${[['-', 16], ['-', 2], ['-', 98], ['+', 100], ['-', 98]]} | ${[3, 2]}
+  `('Base test: $n', ({ initCount, iterations, result }) => {
+    expect(getResult(initCount, iterations)).toStrictEqual(result);
+  });
+});
---
+++ src/698/README.md
@@ -0,0 +1,15 @@
+# Codeforces Round 359 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/698)
+
+[A. Vacations](https://codeforces.com/contest/698/problem/A) - Done
+
+[B. Fix a Tree](https://codeforces.com/contest/698/problem/B)
+
+[C. LRU](https://codeforces.com/contest/698/problem/C)
+
+[D. Limak and Shooting Points](https://codeforces.com/contest/698/problem/D)
+
+[E. Cron](https://codeforces.com/contest/698/problem/E)
+
+[F. Coprime Permutation](https://codeforces.com/contest/698/problem/F)
