4d17e8bb347c4c8ea7959ec451ec7deded0b3efb
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-04-14 11:12:10 +0300

    feat(solution): JTN-1843B

---
+++ src/1843/B.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+  getParams: (array) => {
+    let sum = Math.abs(array[0]);
+    let count = array[0] < 0 ? 1 : 0;
+    let flag = count;
+    for (let j = 1; j < array.length; j++) {
+      sum += Math.abs(array[j]);
+      if (!flag && array[j] < 0) {
+        count++;
+        flag = true;
+      }
+      if (array[j] > 0) {
+        flag = false;
+      }
+    }
+    return [sum, count];
+  },
+};
---
+++ src/1843/B.spec.js
@@ -0,0 +1,14 @@
+﻿const { getParams } = require('./B');
+
+describe('1843B. Long Long', () => {
+  it.each`
+    n    | array                          | result
+    ${1} | ${[-1, 7, -4, -2, 5, -8]}      | ${[27, 3]}
+    ${2} | ${[-1, 0, 0, -2, 1, 0, -3, 0]} | ${[7, 2]}
+    ${3} | ${[2, -1, 0, -3, -7]}          | ${[13, 1]}
+    ${4} | ${[0, -17, 0, 1, 0]}           | ${[18, 1]}
+    ${5} | ${[-1, 0, -2, -1]}             | ${[4, 1]}
+  `('Base test: $n', ({ array, result }) => {
+    expect(getParams(array)).toStrictEqual(result);
+  });
+});
