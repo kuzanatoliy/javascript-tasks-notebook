57619930f9187ab617b2a5003ed08fd30c4d8c96
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-04-19 16:50:59 +0300

    feat(solution): JTN-414A

---
+++ src/414/A.js
@@ -0,0 +1,29 @@
+﻿module.exports = {
+  getInitArray: (length, k) => {
+    if (k === 0 && length === 1) {
+      return [1];
+    }
+
+    const d = Math.floor(length / 2);
+    if (d > k || d === 0) {
+      return -1;
+    }
+    let x = k - d + 1;
+    const res = [x];
+    x *= 2;
+    res.push(x);
+
+    for (let j = 1; j < d; j++) {
+      x++;
+      res.push(x);
+      x++;
+      res.push(x);
+    }
+
+    if (length % 2 === 1) {
+      res.push(++x);
+    }
+
+    return res;
+  },
+};
---
+++ src/414/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 240 (Div. 1)
+
+[Dashboard](https://codeforces.com/contest/414)
+
+[A. Mashmokh and Numbers](https://codeforces.com/contest/414/problem/A) - Done
+
+[B. Mashmokh and ACM](https://codeforces.com/contest/414/problem/B)
+
+[C. Mashmokh and Reverse Operation](https://codeforces.com/contest/414/problem/C)
+
+[D. Mashmokh and Water Tanks](https://codeforces.com/contest/414/problem/D)
+
+[E. Mashmokh's Designed Problem](https://codeforces.com/contest/414/problem/E)
