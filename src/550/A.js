9f30dfb995de5ffe0f9f74b349e69f5b7890e9f6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-05-21 20:49:29 +0300

    fix(solution): JTN-550A

--- src/550/A.js
+++ src/550/A.js
@@ -1,10 +1,10 @@
 ﻿module.exports = {
   isItPossibleToDetermine: (string) => {
-    let newString = string.replace('AB', '').replace('BA', '');
-    if (string.length - newString.length === 4) {
+    let newString = string.replace('AB', '*').replace('BA', '*');
+    if (string.length - newString.length === 2) {
       return 'YES';
     }
-    newString = string.replace('BA', '').replace('AB', '');
-    return string.length - newString.length === 4 ? 'YES' : 'NO';
+    newString = string.replace('BA', '*').replace('AB', '*');
+    return string.length - newString.length === 2 ? 'YES' : 'NO';
   },
 };
--- src/550/A.spec.js
+++ src/550/A.spec.js
@@ -6,6 +6,7 @@ describe('550A. Two Substrings', () => {
     ${1} | ${'ABA'}     | ${'NO'}
     ${2} | ${'BACFAB'}  | ${'YES'}
     ${3} | ${'AXBYBXA'} | ${'NO'}
+    ${4} | ${'ABAB'}    | ${'NO'}
   `('Base test: $n', ({ string, result }) => {
     expect(isItPossibleToDetermine(string)).toStrictEqual(result);
   });
