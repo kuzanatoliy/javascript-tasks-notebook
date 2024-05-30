cfd01dde32bc09f841f6fe0383a84f6b1c566584
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-05-30 19:10:42 +0300

    feat(solution): JTN-1463A

--- src/1463/A.spec.js
+++ src/1463/A.spec.js
@@ -3,10 +3,10 @@
 describe('1463A. Dungeon', () => {
   it.each`
     n    | a     | b    | c    | result
-    ${1} | ${3}  | ${2} | ${4} | ${'NO'}
+    ${1} | ${3}  | ${2} | ${4} | ${'YES'}
     ${2} | ${1}  | ${1} | ${1} | ${'NO'}
     ${3} | ${10} | ${1} | ${7} | ${'NO'}
-  `('Base test: $n', ({ array, result }) => {
-    expect(isItPossibleToWin(array)).toBe(result);
+  `('Base test: $n', ({ a, b, c, result }) => {
+    expect(isItPossibleToWin(a, b, c)).toBe(result);
   });
 });
