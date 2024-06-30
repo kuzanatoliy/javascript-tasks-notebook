02cbee996c408b527e41429ddbf33c3c77006a0c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-06-30 13:11:00 +0300

    feat(solution): JTN-1811A

---
+++ src/1811/A.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+  getNewNumber: (string, number) => {
+    let j = 0;
+    let newString = '';
+    while (j < string.length && +string[j] >= number) {
+      newString += string[j];
+      j++;
+    }
+    newString += number;
+    while (j < string.length) {
+      newString += string[j];
+      j++;
+    }
+    return newString;
+  },
+};
