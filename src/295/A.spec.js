1065d5bc1443170c8a5966fd46dbab5ebdc4668c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-07-18 19:09:21 +0300

    feat(solution): JTN-295A

---
+++ src/295/A.spec.js
@@ -0,0 +1,12 @@
+﻿const { getResultArray } = require('./A');
+
+describe('295A. Greg and Array', () => {
+  it.each`
+    n    | array           | operations                           | queries                                             | result
+    ${1} | ${[1, 2, 3]}    | ${[[1, 2, 1], [1, 3, 2], [2, 3, 4]]} | ${[[1, 2], [1, 3], [2, 3]]}                         | ${[9, 18, 17]}
+    ${2} | ${[1]}          | ${[[1, 1, 1]]}                       | ${[[1, 1]]}                                         | ${[2]}
+    ${3} | ${[1, 2, 3, 4]} | ${[[1, 2, 1], [2, 3, 2], [3, 4, 4]]} | ${[[1, 2], [1, 3], [2, 3], [1, 2], [1, 3], [2, 3]]} | ${[5, 18, 31, 20]}
+  `('Base test: $n', ({ array, operations, queries, result }) => {
+    expect(getResultArray(array, operations, queries)).toStrictEqual(result);
+  });
+});
---
+++ src/295/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 179 (Div. 1)
+
+[Dashboard](https://codeforces.com/contest/295)
+
+[A. Greg and Array](https://codeforces.com/contest/295/problem/A) - Done
+
+[B. Greg and Graph](https://codeforces.com/contest/295/problem/B)
+
+[C. Greg and Friends](https://codeforces.com/contest/295/problem/C)
+
+[D. Greg and Caves](https://codeforces.com/contest/295/problem/D)
+
+[E. Yaroslav and Points](https://codeforces.com/contest/295/problem/E)
