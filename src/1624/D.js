93d567ac9627ac69f13fe11e9529618d2bfdc3b0
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-07-24 18:44:17 +0300

    feat(solution): JTN-1624D

---
+++ src/1624/D.js
@@ -0,0 +1,28 @@
+﻿module.exports = {
+  getMaxLength: (string, k) => {
+    const map = {};
+    for (let j = 0; j < string.length; j++) {
+      map[string[j]] = (map[string[j]] || 0) + 1;
+    }
+    const keys = Object.keys(map);
+    let d = 0;
+    let count = 0;
+    for (let jj = 0; jj < keys.length; jj++) {
+      const dd = map[keys[jj]] % 2;
+      count += (map[keys[jj]] - dd) / 2;
+      d += dd;
+    }
+    const ddd = count % k;
+    count = (count - ddd) / k;
+    if (ddd === 0) {
+      if (d < k) {
+        return count * 2;
+      }
+      return count * 2 + 1;
+    }
+    if (ddd >= k / 2 || d >= k - ddd * 2) {
+      return count * 2 + 1;
+    }
+    return count * 2;
+  },
+};
---
+++ src/1624/D.spec.js
@@ -0,0 +1,19 @@
+﻿const { getMaxLength } = require('./D');
+
+describe('1624D. Palindromes Coloring', () => {
+  it.each`
+    n     | string           | k    | result
+    ${1}  | ${'bxyaxzay'}    | ${2} | ${3}
+    ${2}  | ${'aaaaaa'}      | ${3} | ${2}
+    ${3}  | ${'abcdef'}      | ${1} | ${1}
+    ${4}  | ${'abcdef'}      | ${6} | ${1}
+    ${5}  | ${'dxd'}         | ${2} | ${1}
+    ${6}  | ${'abcabcabcac'} | ${2} | ${5}
+    ${7}  | ${'sipkic'}      | ${6} | ${1}
+    ${8}  | ${'eatoohd'}     | ${2} | ${1}
+    ${9}  | ${'llw'}         | ${1} | ${3}
+    ${10} | ${'bfvfbv'}      | ${2} | ${3}
+  `('Base test: $n', ({ string, k, result }) => {
+    expect(getMaxLength(string, k)).toBe(result);
+  });
+});
