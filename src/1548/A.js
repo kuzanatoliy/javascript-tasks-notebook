c3e4e9e3493102c95c1a987a66b5919c7d582640
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-08-12 21:16:33 +0300

    feat(solution): JTN-1548

---
+++ src/1548/A.js
@@ -0,0 +1,31 @@
+﻿module.exports = {
+  getNumbers: (count, friends, queries) => {
+    const deg = Array(count + 1).fill(0);
+    for (let j = 0; j < friends.length; j++) {
+      ++deg[Math.min(...friends[j])];
+    }
+
+    let ans = 0;
+    for (let u = 1; u <= count; u++) {
+      ans += deg[u] === 0;
+    }
+
+    const res = [];
+
+    for (let q = 0; q < queries.length; q++) {
+      if (queries[q][0] === 1) {
+        if (++deg[Math.min(queries[q][1], queries[q][2])] === 1) {
+          ans--;
+        }
+      } else if (queries[q][0] === 2) {
+        if (--deg[Math.min(queries[q][1], queries[q][2])] === 0) {
+          ans++;
+        }
+      } else {
+        res.push(ans);
+      }
+    }
+
+    return res;
+  },
+};
---
+++ src/1548/README.md
@@ -0,0 +1,15 @@
+# Codeforces Round 736 (Div. 1)
+
+[Dashboard](https://codeforces.com/contest/1548)
+
+[A. Web of Lies](https://codeforces.com/contest/1548/problem/A) - Done
+
+[B. Integers Have Friends](https://codeforces.com/contest/1548/problem/B)
+
+[C. The Three Little Pigs](https://codeforces.com/contest/1548/problem/C)
+
+[D1. Gregor and the Odd Cows (Easy)](https://codeforces.com/contest/1548/problem/D1)
+
+[D2. Gregor and the Odd Cows (Hard)](https://codeforces.com/contest/1548/problem/D2)
+
+[E. Gregor and the Two Painters](https://codeforces.com/contest/1548/problem/E)
