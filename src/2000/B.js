a082850b56caa9c58541b8687a4417fcb75fa23c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-08-18 23:21:18 +0300

    feat(solution): JTN-2000B

---
+++ src/2000/B.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+  isTheAllPassengersFollowedTheRecommendations: (array) => {
+    const arr = new Array(array.length + 2).fill(0);
+    arr[array[0]]++;
+    for (let j = 1; j < array.length; j++) {
+      const index = array[j];
+      if (arr[index - 1] !== 0 || arr[index + 1] !== 0) {
+        arr[index]++;
+        continue;
+      }
+      return 'NO';
+    }
+    return 'YES';
+  },
+};
---
+++ src/2000/B.spec.js
@@ -0,0 +1,13 @@
+﻿const { isTheAllPassengersFollowedTheRecommendations } = require('./B');
+
+describe('2000B. Seating in a Bus', () => {
+  it.each`
+    n    | array              | result
+    ${1} | ${[5, 4, 2, 1, 3]} | ${'NO'}
+    ${2} | ${[2, 3, 1]}       | ${'YES'}
+    ${3} | ${[2, 3, 1, 4]}    | ${'YES'}
+    ${4} | ${[1, 2, 3, 5, 4]} | ${'NO'}
+  `('Base test: $n', ({ array, result }) => {
+    expect(isTheAllPassengersFollowedTheRecommendations(array)).toBe(result);
+  });
+});
