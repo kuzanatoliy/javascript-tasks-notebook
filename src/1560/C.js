b0de37c0ee1830649a741cc11b06c864fb924b18
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-08-28 21:47:07 +0300

    feat(solution): JTN-1560C

---
+++ src/1560/C.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  getPosition: (number) => {
+    let j = 1;
+    while (j < number) {
+      number -= j;
+      j += 2;
+    }
+    const d = Math.ceil(j / 2);
+    return number <= d ? [number, d] : [d, d - (number - d)];
+  },
+};
---
+++ src/1560/C.spec.js
@@ -0,0 +1,16 @@
+﻿const { getPosition } = require('./C');
+
+describe('1560C. Infinity Table', () => {
+  it.each`
+    n    | number        | result
+    ${1} | ${11}         | ${[2, 4]}
+    ${2} | ${14}         | ${[4, 3]}
+    ${3} | ${5}          | ${[1, 3]}
+    ${4} | ${4}          | ${[2, 1]}
+    ${5} | ${1}          | ${[1, 1]}
+    ${6} | ${2}          | ${[1, 2]}
+    ${7} | ${1000000000} | ${[31623, 14130]}
+  `('Base test: $n', ({ number, result }) => {
+    expect(getPosition(number)).toStrictEqual(result);
+  });
+});
