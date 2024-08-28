734db97774b4991cc579f182f54131ab5c360004
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-08-28 21:23:11 +0300

    feat(solution): JTN-1997A

---
+++ src/1997/A.spec.js
@@ -0,0 +1,13 @@
+﻿const { getNewPassword } = require('./A');
+
+describe('1997A. Strong Password', () => {
+  it.each`
+    n    | str           | result
+    ${1} | ${'a'}        | ${'ba'}
+    ${2} | ${'aaa'}      | ${'abaa'}
+    ${3} | ${'abb'}      | ${'abab'}
+    ${4} | ${'password'} | ${'pasasword'}
+  `('Base test: $n', ({ str, result }) => {
+    expect(getNewPassword(str)).toBe(result);
+  });
+});
