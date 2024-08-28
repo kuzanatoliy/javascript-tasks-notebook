172433f5039e1f44d545cfd77cadc3429b1ede7f
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-08-28 21:02:42 +0300

    feat(solution): JTN-1743A

---
+++ src/1743/A.spec.js
@@ -0,0 +1,11 @@
+﻿const { getCountOfCombinations } = require('./A');
+
+describe('1743A. Password', () => {
+  it.each`
+    n    | array                       | result
+    ${1} | ${[0, 1, 2, 4, 5, 6, 8, 9]} | ${6}
+    ${2} | ${[8]}                      | ${216}
+  `('Base test: $n', ({ array, result }) => {
+    expect(getCountOfCombinations(array)).toBe(result);
+  });
+});
