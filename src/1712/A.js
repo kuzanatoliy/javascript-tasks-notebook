274a4ac63b2bd26a8c5e6da1d739aae762b5da84
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-09-29 11:19:21 +0300

    feat(solution): JTN-1712A

---
+++ src/1712/A.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  getCountOfOperations: (arr, k) => {
+    let count = 0;
+    for (let j = 0; j < k; j++) {
+      if (arr[j] > k) {
+        count++;
+      }
+    }
+    return count;
+  },
+};
