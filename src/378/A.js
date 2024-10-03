59c53b42e511e45e70e3df95f55586917861e3ab
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-10-03 21:56:19 +0300

    feat(solution): JTN-378A

---
+++ src/378/A.js
@@ -0,0 +1,21 @@
+﻿module.exports = {
+  getConfigurations: (a, b) => {
+    let c1 = 0;
+    let c2 = 0;
+    let c3 = 0;
+    for (let j = 1; j < 7; j++) {
+      const d1 = Math.abs(j - a);
+      const d2 = Math.abs(j - b);
+      if (d1 < d2) {
+        c1++;
+        continue;
+      }
+      if (d1 === d2) {
+        c2++;
+        continue;
+      }
+      c3++;
+    }
+    return [c1, c2, c3];
+  },
+};
---
+++ src/378/A.spec.js
@@ -0,0 +1,11 @@
+﻿const { getConfigurations } = require('./A');
+
+describe('378A. Playing with Dice', () => {
+  it.each`
+    n    | a    | b    | result
+    ${1} | ${2} | ${5} | ${[3, 0, 3]}
+    ${2} | ${2} | ${4} | ${[2, 1, 3]}
+  `('Base test: $n', ({ a, b, result }) => {
+    expect(getConfigurations(a, b)).toStrictEqual(result);
+  });
+});
