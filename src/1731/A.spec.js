a34b801281bc07a13b233ec68e0fd390379e140e
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-10-30 19:09:24 +0300

    fix(solution): JTN-1731A

--- src/1731/A.spec.js
+++ src/1731/A.spec.js
@@ -2,10 +2,10 @@
 
 describe('1731A. Planets', () => {
   it.each`
-    n    | array                      | result
-    ${1} | ${[2n, 3n, 2n]}            | ${28308n}
-    ${2} | ${[1n, 3n]}                | ${8088n}
-    ${3} | ${[1000000n, 1000000n, 1]} | ${2022000000004044n}
+    n    | array                       | result
+    ${1} | ${[2n, 3n, 2n]}             | ${28308n}
+    ${2} | ${[1n, 3n]}                 | ${8088n}
+    ${3} | ${[1000000n, 1000000n, 1n]} | ${2022000000004044n}
   `('Base test: $n', ({ array, result }) => {
     expect(getMaxMoney(array)).toBe(result);
   });
