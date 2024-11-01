6ca7c1737f3cd76157551a83edeb359353c5b55c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-11-01 17:04:24 +0300

    feat(solution): JTN-2025A

---
+++ src/2025/A.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  getCountOfOperations: (str1, str2) => {
+    let ind = 0;
+    while (str1[ind] && str2[ind] && str1[ind] === str2[ind]) {
+      ind++;
+    }
+    return str1.length + str2.length - ind + (ind > 0 ? 1 : 0);
+  },
+};
---
+++ src/2025/A.spec.js
@@ -0,0 +1,12 @@
+﻿const { getCountOfOperations } = require('./A');
+
+describe('2025A. Two Screens', () => {
+  it.each`
+    n    | str1          | str2               | result
+    ${1} | ${'GARAGE'}   | ${'GARAGEFORSALE'} | ${14}
+    ${2} | ${'ABCDE'}    | ${'AABCD'}         | ${10}
+    ${3} | ${'TRAINING'} | ${'DRAINING'}      | ${16}
+  `('Base test: $n', ({ str1, str2, result }) => {
+    expect(getCountOfOperations(str1, str2)).toBe(result);
+  });
+});
