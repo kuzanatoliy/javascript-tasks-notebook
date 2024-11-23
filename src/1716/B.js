c3c2a7d1ce14bf780a77bdc258bcf0497bcfb551
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-11-23 12:40:28 +0300

    feat(solution): JTN-1716B

---
+++ src/1716/B.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+  getPermutationChain: (num) => {
+    const arr = new Array(num).fill(0).map((_, ind) => ind + 1);
+    const res = [[...arr]];
+    for (let j = 1; j < num; j++) {
+      arr[j - 1] = arr[j];
+      arr[j] = 1;
+      res.push([...arr]);
+    }
+    return res;
+  },
+};
---
+++ src/1716/B.spec.js
@@ -0,0 +1,11 @@
+﻿const { getPermutationChain } = require('./B');
+
+describe('1714B. Permutation Chain', () => {
+  it.each`
+    n    | origin | result
+    ${1} | ${2}   | ${[[1, 2], [2, 1]]}
+    ${2} | ${3}   | ${[[1, 2, 3], [2, 1, 3], [2, 3, 1]]}
+  `('Base test: $n', ({ origin, result }) => {
+    expect(getPermutationChain(origin)).toStrictEqual(result);
+  });
+});
