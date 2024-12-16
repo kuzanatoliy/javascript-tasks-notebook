1b63de53bb2a7959a613690b6fb36837b6329fd4
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-12-16 18:46:31 +0300

    feat(solution): JTN-1834B

---
+++ src/1834/B.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+  getIndexes: (array) => {
+    const arr = new Array(array.length + 1);
+    array.forEach((item, ind) => {
+      arr[item] = ind;
+    });
+    if (arr[1] > arr[array.length] && arr[2] > arr[array.length]) {
+      return [arr[array.length] + 1, Math.min(arr[1], arr[2]) + 1];
+    }
+    if (arr[1] < arr[array.length] && arr[2] < arr[array.length]) {
+      return [arr[array.length] + 1, Math.max(arr[1], arr[2]) + 1];
+    }
+    return [arr[1] + 1, arr[2] + 1];
+  },
+};
