582c7fa77b2dd4e97d5d749c6fc805d1c6ec0693
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-01-27 19:54:03 +0300

    feat(solution): JTN-1966A

--- src/1966/A.spec.js
+++ src/1966/A.spec.js
@@ -1,8 +1,8 @@
-﻿const { getMinCountOfInvites } = require('./A');
+﻿const { getCountOfNumbers } = require('./A');
 
 describe('1966A. Card Exchange', () => {
   it.each`
-    n    | array                             | k     | result
+    n    | arr                               | k     | result
     ${1} | ${[4, 1, 1, 4, 4]}                | ${3}  | ${2}
     ${2} | ${[7]}                            | ${10} | ${1}
     ${3} | ${[4, 2, 1, 100, 5, 2, 3]}        | ${2}  | ${1}
@@ -10,7 +10,7 @@ describe('1966A. Card Exchange', () => {
     ${5} | ${[3, 8, 1, 48, 7]}               | ${2}  | ${5}
     ${6} | ${[10, 20, 30, 10, 20, 40]}       | ${2}  | ${1}
     ${7} | ${[10, 20, 30, 10, 20, 40]}       | ${3}  | ${6}
-  `('Base test: $n', ({ array, result }) => {
-    expect(getMinCountOfInvites(array)).toBe(result);
+  `('Base test: $n', ({ arr, k, result }) => {
+    expect(getCountOfNumbers(arr, k)).toBe(result);
   });
 });
