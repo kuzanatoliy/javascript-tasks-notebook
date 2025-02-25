6bbf9d4a7e5dba34a07d56128d3a8afba4fe3325
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-02-25 23:30:39 +0300

    feat(solution): JTN-1802A

---
+++ src/1802/A.js
@@ -0,0 +1,31 @@
+﻿module.exports = {
+  getVariantsOfLikes: (map) => {
+    const m = map.map((item) => Math.abs(item)).sort((a, b) => a - b);
+    let o = 0;
+    let d = 0;
+    for (let j = 0; j < m.length; j++) {
+      if (m[j] === m[j + 1]) {
+        j++;
+        d++;
+      } else {
+        o++;
+      }
+    }
+    const max = [1];
+    for (let k = 1; k < o + d; k++) {
+      max.push(max[max.length - 1] + 1);
+    }
+    for (let kk = 0; kk < d; kk++) {
+      max.push(max[max.length - 1] - 1);
+    }
+    const min = [];
+    for (let f = 0; f < d; f++) {
+      min.push(1);
+      min.push(0);
+    }
+    for (let ff = 0; ff < o; ff++) {
+      min.push(ff + 1);
+    }
+    return [max, min];
+  },
+};
---
+++ src/1802/README.md
@@ -0,0 +1,17 @@
+# Codeforces Round 857 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/1800)
+
+[A. Likes](https://codeforces.com/contest/1800/problem/A) - Done
+
+[B. Settlement of Guinea Pigs](https://codeforces.com/contest/1800/problem/B)
+
+[C. The Very Beautiful Blanket](https://codeforces.com/contest/1800/problem/C)
+
+[D. Buying gifts](https://codeforces.com/contest/1800/problem/D)
+
+[E. Music Festival](https://codeforces.com/contest/1800/problem/E)
+
+[F. The way home](https://codeforces.com/contest/1800/problem/F)
+
+[G. Gasoline prices](https://codeforces.com/contest/1800/problem/G)
