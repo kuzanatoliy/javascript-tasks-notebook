2ab3ead6b097e9686aac03097e4a6ab65631988e
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-04-09 17:48:56 +0300

    feat(solution): JTN-302A

---
+++ src/302/A.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+  getResults: (array, queries) => {
+    let x = 0;
+    let y = 0;
+    const ans = new Array(queries.length);
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === 1) {
+        x++;
+      } else {
+        y++;
+      }
+    }
+
+    x = Math.min(x, y) * 2;
+
+    for (let i = 0; i < queries.length; i++) {
+      const l = queries[i];
+      ans[i] = l[1] - l[0] + 1 > x || (l[1] - l[0]) % 2 === 0 ? 0 : 1;
+    }
+
+    return ans;
+  },
+};
---
+++ src/302/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 192 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/302)
+
+[A. Eugeny and Array](https://codeforces.com/contest/302/problem/A) - Done
+
+[B. Eugeny and Play List](https://codeforces.com/contest/302/problem/B)
+
+[C. Yaroslav and Sequence](https://codeforces.com/contest/302/problem/C)
+
+[D. Yaroslav and Time](https://codeforces.com/contest/302/problem/D)
+
+[E. Yaroslav and Algorithm](https://codeforces.com/contest/302/problem/E)
