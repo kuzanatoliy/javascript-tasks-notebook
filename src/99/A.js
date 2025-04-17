6dc5f86e26730e087c99acb2c7f4b46c22b47ce4
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-04-17 12:32:03 +0300

    feat(solution): JTN-99A

---
+++ src/99/A.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+  getIntNumber: (snum) => {
+    const parts = snum.split('.');
+    if (parts[0][parts[0].length - 1] === '9') {
+      return 'GOTO Vasilisa.';
+    }
+    const ans = parts[0].split('');
+    const ans2 = parts[1]
+      .split('')
+      .map((item) => +item)
+      .reverse();
+    for (let j = 1; j < ans2.length; j++) {
+      ans2[j] += Math.round(ans2[j] / 10);
+    }
+    ans[ans.length - 1] =
+      +ans[ans.length - 1] + Math.round(parseFloat(ans2[ans2.length - 1] / 10));
+    return ans.join('');
+  },
+};
---
+++ src/99/README.md
@@ -0,0 +1,13 @@
+# Codeforces Beta Round 78 (Div. 2 Only)
+
+[Dashboard](https://codeforces.com/contest/99)
+
+[A. Help Far Away Kingdom](https://codeforces.com/contest/99/problem/A) - Done
+
+[B. Help Chef Gerasim](https://codeforces.com/contest/99/problem/B)
+
+[C. Help Victoria the Wise](https://codeforces.com/contest/99/problem/C)
+
+[D. Help King](https://codeforces.com/contest/99/problem/D)
+
+[E. Help Greg the Dwarf](https://codeforces.com/contest/99/problem/E)
