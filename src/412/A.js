77b5273c4d3c70b4db72cf329e7071b981fcde26
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-04-18 16:09:50 +0300

    feat(solution): JTN-412A

---
+++ src/412/A.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  getMap: (num, arrA, arrB) => {
+    const arr = new Array(num).fill(1);
+    for (let j = 0; j < arrB.length; j++) {
+      arr[arrB[j] - 1] = 2;
+    }
+    return arr;
+  },
+};
---
+++ src/412/A.spec.js
@@ -0,0 +1,11 @@
+﻿const { getMap } = require('./A');
+
+describe('412A. Pasha and Hamsters', () => {
+  it.each`
+    n    | num  | arrA               | arrB         | result
+    ${1} | ${4} | ${[1, 2]}          | ${[2, 3, 4]} | ${[1, 2, 2, 2]}
+    ${2} | ${5} | ${[3, 4, 1, 2, 5]} | ${[2, 3]}    | ${[1, 2, 2, 1, 1]}
+  `('Base test: $n', ({ num, arrA, arrB, result }) => {
+    expect(getMap(num, arrA, arrB)).toStrictEqual(result);
+  });
+});
