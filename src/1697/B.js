cb6d53799e28b9202ddfb567e8194025e46a8094
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-05-05 23:14:23 +0300

    feat(solution): JTN-1697B

---
+++ src/1697/B.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+  getMaxDiscount: (goods, queries) => {
+    const arr = goods.sort((a, b) => b - a);
+    const sums = [arr[0]];
+    for (let j = 1; j < arr.length; j++) {
+      sums.push(sums[j - 1] + arr[j]);
+    }
+    const res = [];
+    for (let jj = 0; jj < queries.length; jj++) {
+      res.push(
+        sums[queries[jj][0] - 1] -
+          (sums[queries[jj][0] - queries[jj][1] - 1] || 0)
+      );
+    }
+    return res;
+  },
+};
---
+++ src/1697/B.spec.js
@@ -0,0 +1,10 @@
+﻿const { getMaxDiscount } = require('./B');
+
+describe('1697B. Promo', () => {
+  it.each`
+    n    | goods              | queries                     | result
+    ${1} | ${[5, 3, 1, 5, 2]} | ${[[3, 2], [1, 1], [5, 3]]} | ${[8, 5, 6]}
+  `('Base test: $n', ({ goods, queries, result }) => {
+    expect(getMaxDiscount(goods, queries)).toStrictEqual(result);
+  });
+});
