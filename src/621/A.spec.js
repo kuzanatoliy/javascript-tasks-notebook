65a59c5ea33c101ddc30692f4d1a7f582a7f4358
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-06-09 00:01:12 +0300

    fix(solution): JTN-621A

--- src/621/A.spec.js
+++ src/621/A.spec.js
@@ -4,7 +4,7 @@ describe("621A. Professor GukiZ's Robot", () => {
   it.each`
     n    | arr                                                        | result
     ${1} | ${[1, 2, 3]}                                               | ${6}
-    ${2} | ${[999999999, 999999999, 999999999, 999999999, 999999999]} | ${3}
+    ${2} | ${[999999999, 999999999, 999999999, 999999999, 999999999]} | ${3999999996}
   `('Base test: $n', ({ arr, result }) => {
     expect(getMaxEvenSum(arr)).toBe(result);
   });
