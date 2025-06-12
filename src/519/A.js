21c2eb77db24a3486cd7e39b247da95d63cd4123
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-06-12 16:59:42 +0300

    feat(solution): JTN-519A

---
+++ src/519/A.js
@@ -0,0 +1,36 @@
+﻿/* eslint-disable sort-keys */
+const weights = {
+  Q: 9,
+  R: 5,
+  B: 3,
+  N: 3,
+  P: 1,
+  q: 9,
+  r: 5,
+  b: 3,
+  n: 3,
+  p: 1,
+};
+
+module.exports = {
+  getWinner: (map) => {
+    let w1 = 0;
+    let w2 = 0;
+    for (let j = 0; j < 8; j++) {
+      for (let jj = 0; jj < 8; jj++) {
+        if (map[j][jj] > 'Z') {
+          w2 += weights[map[j][jj]] || 0;
+          continue;
+        }
+        w1 += weights[map[j][jj]] || 0;
+      }
+    }
+    if (w1 > w2) {
+      return 'White';
+    } else if (w2 > w1) {
+      return 'Black';
+    } else {
+      return 'Draw';
+    }
+  },
+};
---
+++ src/519/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 294 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/519)
+
+[A. A and B and Chess](https://codeforces.com/contest/519/problem/A) - Done
+
+[B. A and B and Compilation Errors](https://codeforces.com/contest/519/problem/B1)
+
+[C. A and B and Team Training](https://codeforces.com/contest/519/problem/C)
+
+[D. A and B and Interesting Substrings](https://codeforces.com/contest/519/problem/D1)
+
+[E. A and B and Lecture Rooms](https://codeforces.com/contest/519/problem/E1)
