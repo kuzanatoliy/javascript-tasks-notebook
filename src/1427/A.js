3263faa27926b57b9d2461ac20fc6785b93a52e6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-06-24 23:30:32 +0300

    fix(solution): JTN-1427A

--- src/1427/A.js
+++ src/1427/A.js
@@ -1,7 +1,14 @@
 ﻿module.exports = {
-  getCountOfSteps: (x1, y1, x2, y2) => {
-    const d1 = Math.abs(x1 - x2);
-    const d2 = Math.abs(y1 - y2);
-    return d1 + d2 + (d1 > 0 && d2 > 0 ? 2 : 0);
+  isItPossibleToTransformArray: (arr) => {
+    const sum = arr.reduce((a, b) => a + b, 0);
+
+    if (sum === 0) {
+      return ['NO'];
+    } else {
+      return [
+        'YES',
+        sum > 0 ? arr.sort((a, b) => b - a) : arr.sort((a, b) => a - b),
+      ];
+    }
   },
 };
--- src/1427/A.spec.js
+++ src/1427/A.spec.js
@@ -1,13 +1,13 @@
-﻿const { getCountOfSteps } = require('./A');
+﻿const { isItPossibleToTransformArray } = require('./A');
 
 describe('1427A. Avoiding Zero', () => {
   it.each`
     n    | arr                          | result
-    ${1} | ${[1, -2, 3, 4]}             | ${['YES', [-4, -2, 1, 3]]}
+    ${1} | ${[1, -2, 3, -4]}            | ${['YES', [-4, -2, 1, 3]]}
     ${2} | ${[0, 0, 0]}                 | ${['NO']}
     ${3} | ${[1, -1, 1, -1, 1]}         | ${['YES', [1, 1, 1, -1, -1]]}
     ${4} | ${[40, -31, -9, 0, 13, -40]} | ${['YES', [-40, -31, -9, 0, 13, 40]]}
   `('Base test: $n', ({ arr, result }) => {
-    expect(getCountOfSteps(arr)).toBe(result);
+    expect(isItPossibleToTransformArray(arr)).toStrictEqual(result);
   });
 });
