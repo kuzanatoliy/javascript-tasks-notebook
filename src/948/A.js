20e7b15cd4129d51b7d1cb0a591776df92134179
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-08-08 16:28:22 +0300

    feat(solution): JTN-948A

---
+++ src/948/A.js
@@ -0,0 +1,21 @@
+﻿module.exports = {
+  transformMap: (map) => {
+    for (let j = 0; j < map.length; j++) {
+      for (let jj = 0; jj < map[j].length; jj++) {
+        if (map[j][jj] === '.') {
+          map[j][jj] = 'D';
+        }
+        if (
+          map[j][jj] === 'W' &&
+          ((map[j - 1] && map[j - 1][jj] === 'S') ||
+            (map[j + 1] && map[j + 1][jj] === 'S') ||
+            map[j][jj - 1] === 'S' ||
+            map[j][jj + 1] === 'S')
+        ) {
+          return ['NO'];
+        }
+      }
+    }
+    return ['YES', map];
+  },
+};
---
+++ src/948/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 470 (rated, Div. 2, based on VK Cup 2018 Round 1)
+
+[Dashboard](https://codeforces.com/contest/948)
+
+[A. Protect Sheep](https://codeforces.com/contest/948/problem/A) - Done
+
+[B. Primal Sport](https://codeforces.com/contest/948/problem/B)
+
+[C. Producing Snow](https://codeforces.com/contest/948/problem/C)
+
+[D. Perfect Security](https://codeforces.com/contest/948/problem/D)
+
+[E. Picking Strings](https://codeforces.com/contest/948/problem/E)
