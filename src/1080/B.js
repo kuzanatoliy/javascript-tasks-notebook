a7e4bd7543ea138f860e28d8d1906cc015c05e08
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-08-13 23:03:35 +0300

    feat(solution): JTN-1080A

---
+++ src/1080/B.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+  getAnswers: (queries) => {
+    const res = [];
+    for (let j = 0; j < queries.length; j++) {
+      res.push(
+        Math.ceil(queries[j][1] / 2) * (-1) ** queries[j][1] -
+          Math.ceil((queries[j][0] - 1) / 2) * (-1) ** (queries[j][0] - 1)
+      );
+    }
+    return res;
+  },
+};
---
+++ src/1080/B.spec.js
@@ -0,0 +1,10 @@
+﻿const { getAnswers } = require('./B');
+
+describe('1080B. Margarite and the best present', () => {
+  it.each`
+    n    | queries                                     | result
+    ${1} | ${[[1, 3], [2, 5], [5, 5], [4, 4], [2, 3]]} | ${[-2, -2, -5, 4, -1]}
+  `('Base test: $n', ({ queries, result }) => {
+    expect(getAnswers(queries)).toStrictEqual(result);
+  });
+});
