d98b534ac9a60d0c608c761d3186ed1517837cb2
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-08-19 18:15:36 +0300

    feat(solution): JTN-373A

---
+++ src/373/A.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+  isItPossibleToClick: (map, k) => {
+    k *= 2;
+    const counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let j = 0; j < map.length; j++) {
+      for (let jj = 0; jj < map[j].length; jj++) {
+        if (map[j][jj] !== '.') {
+          counts[map[j][jj]]++;
+        }
+      }
+    }
+    for (let jjj = 0; jjj < counts.length; jjj++) {
+      if (counts[jjj] > k) {
+        return 'NO';
+      }
+    }
+    return 'YES';
+  },
+};
---
+++ src/373/README.md
@@ -0,0 +1,13 @@
+# Codeforces Round 222 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/378)
+
+[A. Playing with Dice](https://codeforces.com/contest/373/problem/A) - Done
+
+[B. Semifinals](https://codeforces.com/contest/378/problem/B)
+
+[C. Maze](https://codeforces.com/contest/378/problem/C)
+
+[D. Preparing for the Contest](https://codeforces.com/contest/378/problem/D)
+
+[E. Captains Mode](https://codeforces.com/contest/378/problem/E)
