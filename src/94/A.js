fb7de3e8d6435a6b5a4c03e1e6b8fb45180b449c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-08-19 14:29:14 +0300

    feat(solution): JTN-94A

---
+++ src/94/A.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+  restorePassword: (pass, codes) => {
+    const map = {};
+    for (let j = 0; j < codes.length; j++) {
+      map[codes[j]] = j.toString();
+    }
+    return (
+      map[pass.slice(0, 10)] +
+      map[pass.slice(10, 20)] +
+      map[pass.slice(20, 30)] +
+      map[pass.slice(30, 40)] +
+      map[pass.slice(40, 50)] +
+      map[pass.slice(50, 60)] +
+      map[pass.slice(60, 70)] +
+      map[pass.slice(70, 80)]
+    );
+  },
+};
---
+++ src/94/README.md
@@ -0,0 +1,13 @@
+# Codeforces Beta Round 76 (Div. 2 Only)
+
+[Dashboard](https://codeforces.com/contest/94)
+
+[A. Restoring Password](https://codeforces.com/contest/94/problem/A) - Done
+
+[B. Friends](https://codeforces.com/contest/94/problem/B)
+
+[C. Frames](https://codeforces.com/contest/94/problem/C)
+
+[D. End of Exams](https://codeforces.com/contest/94/problem/D)
+
+[E. Azembler](https://codeforces.com/contest/94/problem/E)
