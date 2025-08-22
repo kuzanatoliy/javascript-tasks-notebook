08785e9665df374e29573a0e6fb3bf2de4a049ab
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-08-22 17:24:19 +0300

    fix(solution): JTN-870A

--- src/870/A.js
+++ src/870/A.js
@@ -1,13 +1,26 @@
 ﻿module.exports = {
   getPrettyInt: (arrA, arrB) => {
-    const a = Math.min(...arrA);
-    const b = Math.min(...arrB);
-    if (a === b) {
-      return a.toString();
-    } else if (a < b) {
-      return `${a}${b}`;
-    } else {
-      return `${b}${a}`;
+    const a = new Array(10).fill(0);
+    const b = new Array(10).fill(0);
+    for (let j = 0; j < arrA.length; j++) {
+      a[arrA[j]]++;
     }
+    for (let jj = 0; jj < arrB.length; jj++) {
+      b[arrB[jj]]++;
+    }
+    let ca = 0;
+    let cb = 0;
+    for (let jjj = 0; jjj < a.length && (!ca || !cb); jjj++) {
+      if (a[jjj] === b[jjj] && a[jjj] > 0) {
+        return jjj.toString();
+      }
+      if (a[jjj] && !ca) {
+        ca = jjj.toString();
+      }
+      if (b[jjj] && !cb) {
+        cb = jjj.toString();
+      }
+    }
+    return ca < cb ? ca + cb : cb + ca;
   },
 };
--- src/870/A.spec.js
+++ src/870/A.spec.js
@@ -2,9 +2,10 @@
 
 describe('870A. Search for Pretty Integers', () => {
   it.each`
-    n    | arrA                        | arrB                        | result
-    ${1} | ${[4, 2]}                   | ${[5, 7, 6]}                | ${'25'}
-    ${2} | ${[1, 2, 3, 4, 5, 6, 7, 8]} | ${[8, 7, 6, 5, 4, 3, 2, 1]} | ${'1'}
+    n    | arrA                           | arrB                        | result
+    ${1} | ${[4, 2]}                      | ${[5, 7, 6]}                | ${'25'}
+    ${2} | ${[1, 2, 3, 4, 5, 6, 7, 8]}    | ${[8, 7, 6, 5, 4, 3, 2, 1]} | ${'1'}
+    ${3} | ${[5, 4, 2, 3, 6, 1, 7, 9, 8]} | ${[9]}                      | ${'9'}
   `('Base test: $n', ({ arrA, arrB, result }) => {
     expect(getPrettyInt(arrA, arrB)).toBe(result);
   });
