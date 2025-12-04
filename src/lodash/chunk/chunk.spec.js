a1a8a5587c1feca898829b737cb73fefa620b81d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-12-04 15:51:05 +0300

    feat(lodash): JTN-23 add chunk function

---
+++ src/lodash/chunk/chunk.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undefined */
+const { chunk } = require('./chunk');
+
+describe('2151A. Incremental Subarray', () => {
+  it.each`
+    n    | arr                | size         | result
+    ${1} | ${[1, 2, 3]}       | ${undefined} | ${[[1], [2], [3]]}
+    ${2} | ${[1, 2, 3, 4, 5]} | ${2}         | ${[[1, 2], [3, 4], [5]]}
+    ${3} | ${[1, 2, 3, 4, 5]} | ${3}         | ${[[1, 2, 3], [4, 5]]}
+  `('Base test: $n', ({ arr, size, result }) => {
+    expect(chunk(arr, size)).toStrictEqual(result);
+  });
+});
