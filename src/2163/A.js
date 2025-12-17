b614e50644f719d2b5e331e08374d876360be908
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-12-17 16:23:52 +0300

    feat(solution): JTN-2163A

---
+++ src/2163/A.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  isItPossibleToWin: (arr) => {
+    arr.sort((a, b) => a - b);
+    for (let j = 1; j < arr.length - 1; j += 2) {
+      if (arr[j] !== arr[j + 1]) {
+        return 'NO';
+      }
+    }
+    return 'YES';
+  },
+};
---
+++ src/2163/A.spec.js
@@ -0,0 +1,14 @@
+﻿const { isItPossibleToWin } = require('./A');
+
+describe('2163A. Souvlaki VS. Kalamaki', () => {
+  it.each`
+    n    | arr                | result
+    ${1} | ${[4, 2, 2, 1]}    | ${'YES'}
+    ${2} | ${[1, 1, 1, 1]}    | ${'YES'}
+    ${3} | ${[1, 5, 1, 5, 1]} | ${'YES'}
+    ${4} | ${[1, 2, 3]}       | ${'NO'}
+    ${5} | ${[1, 3, 2, 3, 5]} | ${'NO'}
+  `('Base test: $n', ({ arr, result }) => {
+    expect(isItPossibleToWin(arr)).toBe(result);
+  });
+});
