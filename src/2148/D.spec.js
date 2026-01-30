19f297ec7295c49255e0cd214d390a7c378e8a34
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-01-30 15:06:33 +0300

    fix(solution): JTN-2148D

--- src/2148/D.spec.js
+++ src/2148/D.spec.js
@@ -1,11 +1,11 @@
-﻿const { getMaxSum } = require('./C');
+﻿const { getMaxSum } = require('./D');
 
 describe('2148D. Destruction of the Dandelion Fields', () => {
   it.each`
     n    | arr                                               | result
-    ${1} | ${[2, 4, 6]}                                      | ${2}
-    ${2} | ${[4, 2, 1, 6]}                                   | ${7}
-    ${3} | ${[1000000000, 999999999, 1000000000, 999999999]} | ${6}
+    ${1} | ${[2, 4, 6]}                                      | ${0}
+    ${2} | ${[4, 2, 1, 6]}                                   | ${13}
+    ${3} | ${[1000000000, 999999999, 1000000000, 999999999]} | ${2999999999}
   `('Base test: $n', ({ arr, result }) => {
     expect(getMaxSum(arr)).toBe(result);
   });
