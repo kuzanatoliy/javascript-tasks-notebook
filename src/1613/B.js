88a8a52ec824212d5f3b7f14e89d9d3735f7f7ef
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-02-02 12:37:38 +0300

    refactor(solution): JTN-1613B

--- src/1613/B.js
+++ src/1613/B.js
@@ -1,16 +1,10 @@
 ﻿module.exports = {
   getPairs: (arr) => {
     const pairs = [];
-    arr.sort((a, b) => b - a);
-    const set = new Set(arr);
+    arr.sort((a, b) => a - b);
     const l = Math.floor(arr.length / 2);
-    for (let j = 0; j < arr.length && pairs.length < l; j++) {
-      for (let jj = j + 1; jj < arr.length && pairs.length < l; jj++) {
-        const d = arr[j] % arr[jj];
-        if (!set.has(d)) {
-          pairs.push([arr[j], arr[jj]]);
-        }
-      }
+    for (let j = 1; j < arr.length && pairs.length < l; j++) {
+      pairs.push([arr[j], arr[0]]);
     }
     return pairs;
   },
