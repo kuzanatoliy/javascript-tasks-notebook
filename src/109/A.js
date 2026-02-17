8e0f73ee69a4a28c91e4b2cfe6dbdcf2d265638d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-02-17 18:24:26 +0300

    feat(solution): JTN-109A

---
+++ src/109/A.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+  getLukyNumber: (num) => {
+    let four = Math.floor(num / 4);
+    const d = num % 4;
+    let seven = 0;
+    if (four >= 1 && d === 3) {
+      seven = 1;
+      four--;
+    } else if (four >= 3 && d === 2) {
+      seven = 2;
+      four -= 3;
+    } else if (four >= 5 && d === 1) {
+      seven = 3;
+      four -= 5;
+    } else if (d !== 0) {
+      return -1;
+    }
+    seven += Math.floor(four / 7) * 4;
+    four %= 7;
+    return `${new Array(four).fill(4).join('')}${new Array(seven).fill(7).join('')}`;
+  },
+};
---
+++ src/109/A.spec.js
@@ -0,0 +1,12 @@
+﻿const { getLukyNumber } = require('./A');
+
+describe('109A. Lucky Sum of Digits', () => {
+  it.each`
+    n    | num   | result
+    ${1} | ${11} | ${'47'}
+    ${2} | ${10} | ${-1}
+    ${3} | ${64} | ${'4477777777'}
+  `('Base test: $n', ({ num, result }) => {
+    expect(getLukyNumber(num)).toBe(result);
+  });
+});
