6281998bbc681a0164a87c3d43e4f62cadafc80a
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-02-25 17:19:34 +0300

    fix(solution): JTN-672B

--- src/672/B.spec.js
+++ src/672/B.spec.js
@@ -1,11 +1,11 @@
-﻿const { getCountOfOperations } = require('./A');
+﻿const { getCountOfOperations } = require('./B');
 
 describe('672B. Different is Good', () => {
   it.each`
     n    | str        | result
-    ${1} | ${'aa'}    | ${'1'}
-    ${2} | ${'koko'}  | ${'2'}
-    ${3} | ${'murat'} | ${'0'}
+    ${1} | ${'aa'}    | ${1}
+    ${2} | ${'koko'}  | ${2}
+    ${3} | ${'murat'} | ${0}
   `('Base test: $n', ({ str, result }) => {
     expect(getCountOfOperations(str)).toBe(result);
   });
