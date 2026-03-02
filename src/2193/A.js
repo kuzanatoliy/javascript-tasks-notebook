b07d7ff3d9050f6d994cf72a507776a8ffd3256d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-03-02 18:34:24 +0300

    feat(solution): JTN-2193A

---
+++ src/2193/A.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+  isItPossibleToTransform: (s, x, arr) => {
+    const sum = arr.reduce((a, b) => a + b, 0);
+    return s < sum || (s - sum) % x > 0 ? 'NO' : 'YES';
+  },
+};
---
+++ src/2193/A.spec.js
@@ -0,0 +1,15 @@
+﻿const { isItPossibleToTransform } = require('./A');
+
+describe('2175C. Replace and Sum', () => {
+  it.each`
+    n    | s     | x    | arr             | result
+    ${1} | ${3}  | ${5} | ${[1, 1, 1]}    | ${'YES'}
+    ${2} | ${8}  | ${2} | ${[1, 2, 3]}    | ${'YES'}
+    ${3} | ${7}  | ${2} | ${[1, 1, 1, 1]} | ${'NO'}
+    ${4} | ${15} | ${1} | ${[2, 4, 10]}   | ${'NO'}
+    ${5} | ${3}  | ${5} | ${[1, 1, 1]}    | ${'YES'}
+    ${6} | ${8}  | ${2} | ${[1, 2, 3]}    | ${'YES'}
+  `('Base test: $n', ({ s, x, arr, result }) => {
+    expect(isItPossibleToTransform(s, x, arr)).toBe(result);
+  });
+});
