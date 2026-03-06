acd75cc1731ecab36a88931da2fe471d5c9bf050
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-03-06 15:44:29 +0300

    refactor: JTN-22 rename lodash helpers

--- src/lodash/difference-with/difference-with.spec.js
+++ src/lodash/difference-with/difference-with.spec.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-undefined */
-const { differenceBy } = require('./differenceBy');
+const { differenceWith } = require('./difference-with');
 
-describe('Lodash: differenceBy function', () => {
+describe('Lodash: differenceWith function', () => {
   it.each`
     n    | array                   | values        | iteratee      | result
     ${1} | ${[1, 2, 3]}            | ${[2]}        | ${undefined}  | ${[1, 3]}
     ${2} | ${[{ x: 2 }, { x: 1 }]} | ${[{ x: 1 }]} | ${'x'}        | ${[{ x: 2 }]}
     ${3} | ${[2.1, 1.2]}           | ${[2.3, 3.4]} | ${Math.floor} | ${[1.2]}
   `('Base test: $n', ({ array, values, iteratee, result }) => {
-    expect(differenceBy(array, values, iteratee)).toStrictEqual(result);
+    expect(differenceWith(array, values, iteratee)).toStrictEqual(result);
   });
 });
