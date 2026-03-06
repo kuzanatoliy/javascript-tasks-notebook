a07b7026ec505ae75c534114ad217699fb937645
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-03-06 16:34:35 +0300

    feat(lodash): JTN-35 implement findIndex functiobn

--- src/lodash/find-index/find-index.spec.js
+++ src/lodash/find-index/find-index.spec.js
@@ -12,12 +12,12 @@ const FUNCTION_CONDITION = function (o) {
 
 describe('Lodash: findIndex function', () => {
   it.each`
-    n    | users    | condition                          | result
+    n    | arr      | condition                          | result
     ${1} | ${USERS} | ${FUNCTION_CONDITION}              | ${0}
     ${2} | ${USERS} | ${{ active: false, user: 'fred' }} | ${1}
     ${3} | ${USERS} | ${['active', false]}               | ${0}
-    ${3} | ${USERS} | ${'active'}                        | ${2}
-  `('Base test: $n', ({ arr, value, start, end, result }) => {
-    expect(findIndex(arr, value, start, end)).toStrictEqual(result);
+    ${3} | ${USERS} | ${'active'}                        | ${0}
+  `('Base test: $n', ({ arr, condition, result }) => {
+    expect(findIndex(arr, condition)).toBe(result);
   });
 });
