5588d4bd0640d22988475122a4ebb36dd1a5cd19
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-04-03 12:13:57 +0300

    feat(solution): JTN-1248A

---
+++ src/1248/A.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+  getCountOfLinePairs: (arrP, arrQ) => {
+    const p = arrP.reduce((res, val) => res + (val % 2), 0);
+    const q = arrQ.reduce((res, val) => res + (val % 2), 0);
+    return p * q + (arrP.length - p) * (arrQ.length - q);
+  },
+};
---
+++ src/1248/A.spec.js
@@ -0,0 +1,12 @@
+﻿const { getCountOfLinePairs } = require('./A');
+
+describe('1248A. Integer Points', () => {
+  it.each`
+    n    | arrP         | arrQ      | result
+    ${1} | ${[1, 3, 2]} | ${[0, 3]} | ${3}
+    ${2} | ${[1]}       | ${[1]}    | ${1}
+    ${3} | ${[2]}       | ${[1]}    | ${0}
+  `('Base test: $n', ({ arrP, arrQ, result }) => {
+    expect(getCountOfLinePairs(arrP, arrQ)).toBe(result);
+  });
+});
