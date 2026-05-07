004734febba09a05e9b506e5e1969fd857a4f05f
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-05-07 17:03:16 +0300

    feat(solution): JTN-2200C

---
+++ src/2200/C.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+  isItPossibleToExecute: (map) => {
+    const stack = [];
+    let ind = -1;
+    for (let j = 1; j < map.length; j++) {
+      if (stack[ind] === map[j]) {
+        ind--;
+      } else {
+        ind++;
+        stack[ind] = map[j];
+      }
+    }
+    return ind ? 'NO' : 'YES';
+  },
+};
---
+++ src/2200/C.spec.js
@@ -0,0 +1,15 @@
+﻿const { isItPossibleToExecute } = require('./C');
+
+describe('2200B. Deletion Sort', () => {
+  it.each`
+    n    | map               | result
+    ${1} | ${'a'}            | ${'NO'}
+    ${2} | ${'llmllm'}       | ${'YES'}
+    ${3} | ${'uwuuwu'}       | ${'YES'}
+    ${4} | ${'byebye'}       | ${'NO'}
+    ${5} | ${'oooioi'}       | ${'NO'}
+    ${6} | ${'siixxsevvenn'} | ${'YES'}
+  `('Base test: $n', ({ map, result }) => {
+    expect(isItPossibleToExecute(map)).toBe(result);
+  });
+});
