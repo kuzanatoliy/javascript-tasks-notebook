9286035dcd5a5a6ec9d22fe1abd4d9343d85e3f6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-05-12 16:46:31 +0300

    feat(solution): JTN-831A

---
+++ src/831/A.js
@@ -0,0 +1,26 @@
+﻿module.exports = {
+  isUnimodal: (arr) => {
+    let i = 1;
+    let [prev, cur] = arr;
+
+    while (prev < cur) {
+      i++;
+      prev = cur;
+      cur = arr[i];
+    }
+
+    while (prev === cur) {
+      i++;
+      prev = cur;
+      cur = arr[i];
+    }
+
+    while (prev > cur) {
+      i++;
+      prev = cur;
+      cur = arr[i];
+    }
+
+    return i === arr.length ? 'YES' : 'NO';
+  },
+};
---
+++ src/831/A.spec.js
@@ -0,0 +1,14 @@
+﻿const { isUnimodal } = require('./A');
+
+describe('831A. Unimodal Array', () => {
+  it.each`
+    n    | arr                      | result
+    ${1} | ${[1, 5, 5, 5, 4, 2]}    | ${'YES'}
+    ${2} | ${[10, 20, 30, 20, 10]}  | ${'YES'}
+    ${2} | ${[1, 2, 1, 2]}          | ${'NO'}
+    ${2} | ${[3, 3, 3, 3, 3, 3, 3]} | ${'YES'}
+    ${2} | ${[5, 5, 6, 6, 1]}       | ${'NO'}
+  `('Base test: $n', ({ arr, result }) => {
+    expect(isUnimodal(arr)).toBe(result);
+  });
+});
