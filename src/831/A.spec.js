1a2be3e904c4a3481e8b80d04214ddecb53cafb0
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-05-13 15:06:36 +0300

    feat(solution): JTN-831A

--- src/831/A.spec.js
+++ src/831/A.spec.js
@@ -1,13 +1,13 @@
 ﻿const { isUnimodal } = require('./A');
 
-describe('831A. Unimodal Array', () => {
+describe('777A. Unimodal Array', () => {
   it.each`
     n    | arr                      | result
     ${1} | ${[1, 5, 5, 5, 4, 2]}    | ${'YES'}
     ${2} | ${[10, 20, 30, 20, 10]}  | ${'YES'}
-    ${2} | ${[1, 2, 1, 2]}          | ${'NO'}
-    ${2} | ${[3, 3, 3, 3, 3, 3, 3]} | ${'YES'}
-    ${2} | ${[5, 5, 6, 6, 1]}       | ${'NO'}
+    ${3} | ${[1, 2, 1, 2]}          | ${'NO'}
+    ${4} | ${[3, 3, 3, 3, 3, 3, 3]} | ${'YES'}
+    ${5} | ${[5, 5, 6, 6, 1]}       | ${'NO'}
   `('Base test: $n', ({ arr, result }) => {
     expect(isUnimodal(arr)).toBe(result);
   });
