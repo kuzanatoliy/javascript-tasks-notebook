7ce30b9c6078792a36028a878e8df865a817f9bf
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-06-02 23:21:18 +0300

    feat(lodash): JTN-37 add flatten helper

---
+++ src/lodash/flatten/flatten.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undefined */
+const { flatten } = require('./flatten');
+
+describe('Lodash: flatten function', () => {
+  it.each`
+    n    | arr                      | result
+    ${1} | ${[1, [2, [3, [4]], 5]]} | ${[1, 2, [3, [4]], 5]}
+  `('Base test: $n', ({ arr, size, result }) => {
+    expect(flatten(arr, size)).toStrictEqual(result);
+  });
+});
