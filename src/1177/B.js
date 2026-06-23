6b900c30ae442f22bece458dada6463b602e9009
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-06-23 14:29:04 +0300

    fix(solution): JTN-1177B

---
+++ src/1177/B.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+  getPositionValue: (num) => {
+    let j = 1;
+    let js = '1';
+    while (js.length < num) {
+      num -= js.length;
+      j++;
+      js = j.toString();
+    }
+    return js[num - 1];
+  },
+};
---
+++ src/1177/B.spec.js
@@ -0,0 +1,11 @@
+﻿const { getPositionValue } = require('./B');
+
+describe('1177B. Digits Sequence (Hard Edition)', () => {
+  it.each`
+    n    | num   | result
+    ${1} | ${7}  | ${'7'}
+    ${2} | ${21} | ${'5'}
+  `('Base test: $n', ({ num, result }) => {
+    expect(getPositionValue(num)).toBe(result);
+  });
+});
