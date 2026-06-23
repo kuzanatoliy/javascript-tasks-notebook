0a24a850f6f847d1316cd14a8c39ebbb01103d05
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-06-23 13:50:25 +0300

    feat(solution): JTN-1177A

---
+++ src/1177/A.spec.js
@@ -0,0 +1,11 @@
+﻿const { getPositionValue } = require('./A');
+
+describe('1177A. Digits Sequence (Easy Edition)', () => {
+  it.each`
+    n    | num   | result
+    ${1} | ${7}  | ${'7'}
+    ${2} | ${21} | ${'5'}
+  `('Base test: $n', ({ num, result }) => {
+    expect(getPositionValue(num)).toBe(result);
+  });
+});
---
+++ src/1177/README.md
@@ -0,0 +1,7 @@
+# Testing Round 15 (Unrated)
+
+[Dashboard](https://codeforces.com/contest/1177)
+
+[A. Digits Sequence (Easy Edition)](https://codeforces.com/contest/1177/problem/A) - Done
+
+[B. Digits Sequence (Hard Edition)](https://codeforces.com/contest/1177/problem/B)
