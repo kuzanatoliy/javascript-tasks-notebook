eea518ed015017d076da6fb527f0d1032c0a9be6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-07-01 14:11:06 +0300

    feat(solution): JTN-2236B

---
+++ src/2236/B.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  isItPossibleToTransform: (k, str) => {
+    const arr = new Array(k).fill(0);
+    for (let j = 0; j < str.length; j++) {
+      arr[j % k] += str[j] === '1';
+    }
+    return arr.filter((item) => !(item % 2)).length === k ? 'YES' : 'NO';
+  },
+};
---
+++ src/2236/B.spec.js
@@ -0,0 +1,14 @@
+﻿const { isItPossibleToTransform } = require('./B');
+
+describe('2236B. Tatar TV Show', () => {
+  it.each`
+    n    | k    | str       | result
+    ${1} | ${2} | ${'1010'} | ${'YES'}
+    ${2} | ${2} | ${'111'}  | ${'NO'}
+    ${3} | ${3} | ${'111'}  | ${'NO'}
+    ${4} | ${1} | ${'110'}  | ${'YES'}
+    ${5} | ${1} | ${'1'}    | ${'NO'}
+  `('Base test: $n', ({ k, str, result }) => {
+    expect(isItPossibleToTransform(k, str)).toBe(result);
+  });
+});
