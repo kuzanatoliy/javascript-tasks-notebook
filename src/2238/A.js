afe41f36b525edf67202f5845e9a5fdc9eed3c46
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-07-01 18:14:14 +0300

    feat(solution): JTN-2238A

---
+++ src/2238/A.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+  getMinAmount: (c, arrA, arrB) => {
+    const sArrA = [...arrA].sort((a, b) => a - b);
+    const sArrB = [...arrB].sort((a, b) => a - b);
+    let sum1 = 0;
+    let sum2 = 0;
+    for (let jj = 0; jj < sArrA.length; jj++) {
+      if (sArrA[jj] < sArrB[jj]) {
+        return -1;
+      }
+      sum2 += sArrA[jj] - sArrB[jj];
+    }
+    for (let j = 0; j < arrA.length; j++) {
+      if (arrA[j] < arrB[j]) {
+        sum1 = Number.MAX_SAFE_INTEGER;
+        break;
+      }
+      sum1 += arrA[j] - arrB[j];
+    }
+    return Math.min(sum1, sum2 + c);
+  },
+};
---
+++ src/2238/README.md
@@ -0,0 +1,15 @@
+# Codeforces Round 1106 (Div. 2)
+
+[Dashboard](https://codeforces.com/contest/2238)
+
+[A. Another Puzzle from Papyrus](https://codeforces.com/contest/2238/problem/A) - Done
+
+[B. Crimson Triples](https://codeforces.com/contest/2238/problem/B)
+
+[C. Village Guilds](https://codeforces.com/contest/2238/problem/C)
+
+[D. Storming Arasaka](https://codeforces.com/contest/2238/problem/D)
+
+[E. Cake Trial](https://codeforces.com/contest/2238/problem/E)
+
+[F. Infinite Work](https://codeforces.com/contest/2238/problem/F)
