4088978580263474ef9c06e4829b12af59cb2b36
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-07-14 11:15:16 +0300

    feat(solution): JTN-1791G1

---
+++ src/1791/G1.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+  getTeleportsCount: (c, arr) => {
+    const temp = arr.map((item, ind) => item + ind + 1).sort((a, b) => a - b);
+    let count = 0;
+    let lim = c;
+    for (let j = 0; j < temp.length && lim >= temp[j]; j++) {
+      count++;
+      lim -= temp[j];
+    }
+
+    return count;
+  },
+};
