ba8e5e82dfa82c41025c39481bebd5060b8b186f
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-07-31 16:51:18 +0300

    refactor(interviews): JTN-000 getBoxesCount

--- src/interviews/get-boxes-count/get-boxes-count.js
+++ src/interviews/get-boxes-count/get-boxes-count.js
@@ -3,6 +3,9 @@ module.exports = {
     let count = 0;
     let size = 0;
     for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > volume) {
+        return -1;
+      }
       size += arr[j];
       if (size > volume) {
         count++;
--- src/interviews/get-boxes-count/get-boxes-count.spec.js
+++ src/interviews/get-boxes-count/get-boxes-count.spec.js
@@ -3,8 +3,9 @@ const { getBoxesCount } = require('./get-boxes-count');
 
 describe('getBoxesCount', () => {
   it.each`
-    n    | arr                                | volume | result
-    ${1} | ${[1, 3, 2, 4, 10, 8, 4, 2, 5, 3]} | ${12}  | ${4}
+    n    | arr                                 | volume | result
+    ${1} | ${[1, 3, 2, 4, 10, 8, 4, 2, 5, 3]}  | ${12}  | ${4}
+    ${2} | ${[1, 3, 2, 4, 10, 8, 4, 2, 14, 3]} | ${12}  | ${-1}
   `('Base test: $n', ({ arr, volume, result }) => {
     expect(getBoxesCount(arr, volume)).toBe(result);
   });
