8f6a220d7f4e00c4e2d68b4885e45b7d0679d941
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-08-02 17:48:23 +0300

    feat(solution): JTN-000 isPolindromeTextV1

--- src/interviews/is-palindrome/is-palindrome.js
+++ src/interviews/is-palindrome/is-palindrome.js
@@ -1,3 +1,10 @@
 module.exports = {
   isPalindrome: (str) => str === str.split('').reverse().join(''),
+  isPolindromeTextV1: (text) => {
+    const temp = text
+      .toLowerCase()
+      .split('')
+      .filter((item) => item >= 'a' && item <= 'z');
+    return temp.join('') === temp.reverse('').join('');
+  },
 };
--- src/interviews/is-palindrome/is-palindrome.spec.js
+++ src/interviews/is-palindrome/is-palindrome.spec.js
@@ -1,4 +1,4 @@
-const { isPalindrome } = require('./is-palindrome');
+const { isPalindrome, isPolindromeTextV1 } = require('./is-palindrome');
 
 describe('isPalindrome', () => {
   it.each`
@@ -7,5 +7,15 @@ describe('isPalindrome', () => {
     ${2} | ${'abcba'} | ${true}
   `('Base test: $n', ({ str, result }) => {
     expect(isPalindrome(str)).toBe(result);
+  });
+});
+
+describe('isPolindromeTextV1', () => {
+  it.each`
+    n    | str                                      | result
+    ${1} | ${'A man, a plan, a canal: Panama blue'} | ${false}
+    ${2} | ${'A man, a plan, a canal: Panama'}      | ${true}
+  `('Base test: $n', ({ str, result }) => {
+    expect(isPolindromeTextV1(str)).toBe(result);
   });
 });
