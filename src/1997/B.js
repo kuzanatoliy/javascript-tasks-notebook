426a8aac748062a798aaa77fb231e1dc591f5cb0
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-08-27 13:53:08 +0300

    feat(solution): JTN-1997B

---
+++ src/1997/B.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+  getCellCount: (map) => {
+    let count = 0;
+    for (let j = 2; j < map[0].length; j++) {
+      count +=
+        map[0][j - 2] === 'x' &&
+        map[0][j - 1] === '.' &&
+        map[0][j] === 'x' &&
+        map[1][j - 2] === '.' &&
+        map[1][j - 1] === '.' &&
+        map[1][j] === '.';
+      count +=
+        map[0][j - 2] === '.' &&
+        map[0][j - 1] === '.' &&
+        map[0][j] === '.' &&
+        map[1][j - 2] === 'x' &&
+        map[1][j - 1] === '.' &&
+        map[1][j] === 'x';
+    }
+    return count;
+  },
+};
---
+++ src/1997/B.spec.js
@@ -0,0 +1,13 @@
+﻿const { getCellCount } = require('./B');
+
+describe('1997A. Strong Password', () => {
+  it.each`
+    n    | map                           | result
+    ${1} | ${['.......x', '.x.xx...']}   | ${1}
+    ${2} | ${['..', '..']}               | ${0}
+    ${3} | ${['xxx', 'xxx']}             | ${0}
+    ${4} | ${['..x.x.x.x', 'x.......x']} | ${2}
+  `('Base test: $n', ({ map, result }) => {
+    expect(getCellCount(map)).toBe(result);
+  });
+});
